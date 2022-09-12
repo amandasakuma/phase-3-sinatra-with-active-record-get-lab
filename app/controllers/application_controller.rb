660874c2ebde40834114364eda3d01cb84ac0fed
Amanda Sakuma <amandasakuma@gmail.com>
2022-09-12 17:05:58 -0400

    commit tree 0dc3830110bce91180d11c1cb3cd181944cfbd25

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+set :default_content_type, 'application/json'
 
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+  get 'bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
 end
